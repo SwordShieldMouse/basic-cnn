7683412f00b5a83b4bdbf54326190a5b500a5a54
SwordShieldMouse <alke8882000@gmail.com>
2018-12-24 22:38:03 -0700

    added test/train code

---
+++ main.py
@@ -0,0 +1,91 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+
+device = torch.device('cuda' if torch.cuda.is_available() else "cpu")
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+
+        self.conv1 = nn.Conv2d(3, 10, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(10, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.leaky_relu(self.conv1(x)))
+        x = self.pool(F.leaky_relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.leaky_relu(self.fc1(x))
+        x = F.leaky_relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+cnn = CNN().to(device)
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+train_set = torchvision.datasets.CIFAR10(root = "./data", train = True, download = True, transform = transform)
+
+trainloader = DataLoader(train_set, batch_size = 5, shuffle = True, num_workers = 2)
+
+testset = torchvision.datasets.CIFAR10(root='./data', train=False,
+                                       download=True, transform=transform)
+testloader = torch.utils.data.DataLoader(testset, batch_size=4,
+                                         shuffle=False, num_workers=2)
+
+classes = ('plane', 'car', 'bird', 'cat',
+           'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
+
+criterion = nn.CrossEntropyLoss()
+optim = torch.optim.Adam(cnn.parameters(), lr = 1e-4)
+
+epochs = 10
+
+# train
+print("training")
+for epoch in range(epochs):
+    print("epoch {}".format(epoch))
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        inputs, labels = data
+
+        inputs, labels = inputs.to(device), labels.to(device)
+
+        optim.zero_grad()
+
+        outputs = cnn(inputs)
+
+        loss = criterion(outputs, labels)
+
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+        if i % 2000 == 1999:    # print every 2000 mini-batches
+            print('[epoch {}, mini-batch {}] loss: {}'.format(epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print("finished training")
+
+# test
+correct = 0
+total = 0
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        images, labels = images.to(device), labels.to(device)
+        outputs = cnn(images)
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item() # if we test in a batch
+
+print("total accuracy is {}".format(correct / total))
